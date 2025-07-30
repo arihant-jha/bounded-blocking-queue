2401333d407c0e440f4f1c8f1122beb062e596f6
axh <iamarihantjha@gmail.com>
2025-07-31 02:38:24 +0530

    ran experiment with 10k threads

--- b.py
+++ b.py
@@ -1,60 +1,79 @@
-#Sketching out the class
+'''
+1. initializing the max connections at the start
+2. python stopped thread creation at 10k threads.
+3. But the experiment ran successfully!
+'''
+import time
+from queue import Queue
 import psycopg2
 import os
-from queue import Queue #by default bounded if provided the maxsize param
 from concurrent.futures import ThreadPoolExecutor
 
+
 class ConnectionPool:
-    def __init__(self, min_size, max_size, db_config) -> None:
+    def __init__(self, db_config, max_size) -> None:
         self.db_config = db_config
-        self.min_size = min_size
         self.max_size = max_size
-        self.connections = 0
         self.queue = Queue(max_size)
 
-        # Creating initial connections - I want 5 min connections to prevent cold start
-        for _ in range(5):
+        for _ in range(max_size):
             conn = self._get_new_connection()
             self.queue.put(conn)
 
-    def _get_new_connection(self):
-        if self.connections < self.max_size:
-            self.connections += 1
-            conn = psycopg2.connect(**self.db_config)
-            return conn
-        return None
-
     def get(self):
-        "Returns a connection string when it becomes available"
-        # return self.queue.get()
-        if self.queue.not_empty():
-            return self.queue.get()
-        conn = self._get_new_connection()
-        if conn is not None:
-            return conn
-    
+        return self.queue.get()
+        
     def put(self, conn):
         self.queue.put(conn)
-   
+
+    def _get_new_connection(self):
+        conn = psycopg2.connect(**self.db_config)
+        return conn
+    
+def get_pool(max_size):
+    DB_CONFIG = {
+            "dbname": os.environ["PG_DB"],
+            "user": os.environ["PG_USER"],
+            "password": os.environ["PG_PASSWORD"],
+            "host": "localhost",
+            "port": int(os.environ["PG_PORT"]),
+        }
+    pool = ConnectionPool(db_config=DB_CONFIG, max_size=max_size)
+    return pool
    
 def simmulate_query(conn):
     cur = conn.cursor()
     cur.execute("SELECT pg_sleep(0.001)")
     return True
-     
-def test_connection_pool():
-    DB_CONFIG = {
-        "dbname": os.environ["PG_DB"],
-        "user": os.environ["PG_USER"],
-        "password": os.environ["PG_PASSWORD"],
-        "host": "localhost",
-        "port": int(os.environ["PG_PORT"]),
-    }
-    pool = ConnectionPool(5, 100, DB_CONFIG)
-    with ThreadPoolExecutor(max_workers=500) as exec:
-        for _ in range(500):
-            futures = exec.submit(simmulate_query, pool.get)
-            print(futures.result())
 
-            
-test_connection_pool()
+
+
+def worker(pool):
+    conn = pool.get()
+    simmulate_query(conn)
+    pool.put(conn)
+    
+def test_connection_pool():
+    parallel_threads = 10000
+    max_size = 100
+    
+    pool = get_pool(max_size)
+    start_time = time.time()
+
+
+    with ThreadPoolExecutor(max_workers=parallel_threads) as exec:
+        # conn = pool.get()
+        # exec.submit(simmulate_query, conn)
+        # pool.put(conn)
+        futures = [exec.submit(worker, pool) for _ in range(parallel_threads)]
+        for f in futures:
+            f.result()
+    
+    end_time = time.time()
+    print(end_time - start_time)
+
+    
+if __name__ == "__main__":
+    # pool = get_pool()
+    # simmulate_query(pool.get())
+    test_connection_pool()
--- c.py
+++ c.py
@@ -1,11 +1,60 @@
 #Sketching out the class
+import psycopg2
+import os
 from queue import Queue #by default bounded if provided the maxsize param
+from concurrent.futures import ThreadPoolExecutor
 
 class ConnectionPool:
-    def __init__(self, min_size, max_size) -> None:
+    def __init__(self, min_size, max_size, db_config) -> None:
+        self.db_config = db_config
         self.min_size = min_size
         self.max_size = max_size
+        self.connections = 0
         self.queue = Queue(max_size)
+
+        # Creating initial connections - I want 5 min connections to prevent cold start
+        for _ in range(5):
+            conn = self._get_new_connection()
+            self.queue.put(conn)
+
+    def _get_new_connection(self):
+        if self.connections < self.max_size:
+            self.connections += 1
+            conn = psycopg2.connect(**self.db_config)
+            return conn
+        return None
+
+    def get(self):
+        "Returns a connection string when it becomes available"
+        # return self.queue.get()
+        if self.queue.not_empty():
+            return self.queue.get()
+        conn = self._get_new_connection()
+        if conn is not None:
+            return conn
     
     def put(self, conn):
         self.queue.put(conn)
+   
+   
+def simmulate_query(conn):
+    cur = conn.cursor()
+    cur.execute("SELECT pg_sleep(0.001)")
+    return True
+     
+def test_connection_pool():
+    DB_CONFIG = {
+        "dbname": os.environ["PG_DB"],
+        "user": os.environ["PG_USER"],
+        "password": os.environ["PG_PASSWORD"],
+        "host": "localhost",
+        "port": int(os.environ["PG_PORT"]),
+    }
+    pool = ConnectionPool(5, 100, DB_CONFIG)
+    with ThreadPoolExecutor(max_workers=500) as exec:
+        for _ in range(500):
+            futures = exec.submit(simmulate_query, pool.get)
+            print(futures.result())
+
+            
+test_connection_pool()
